be5ba8097760d9997a95823b3baa51eb19fee1d7
Blake McHale <mchale.blake@gmail.com>
2022-01-08 12:33:45 -0500

    Start tf processor and commander in launch

--- robot_bringup/launch/run_vehicles.py
+++ robot_bringup/launch/run_vehicles.py
@@ -2,12 +2,14 @@
 from enum import IntEnum
 import os
 from ament_index_python.packages import get_package_share_directory
-from ros2_utils.launch import get_local_arguments
+from ros2_utils.launch import get_local_arguments, combine_names
 from robot_control.launch.run_vehicles import LAUNCH_ARGS as R_LAUNCH_ARGS, launch_setup as r_launch_setup, extract_namespaces
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions.node import Node
 
+
+robot_command = get_package_share_directory("robot_command")
 
 LAUNCH_ARGS = R_LAUNCH_ARGS
 
@@ -16,7 +18,21 @@ def launch_setup(context, *args, **kwargs):
     """
     ld = r_launch_setup(context, args, kwargs)
     largs = get_local_arguments(LAUNCH_ARGS, context)
-    # namespaces = extract_namespaces(largs)
-    # for i, namespace in enumerate(namespaces):
-    #     pass
+    namespaces = extract_namespaces(largs)
+    log_level = largs["log_level"].upper()
+    for i, namespace in enumerate(namespaces):
+        commander_node = combine_names([namespace, "commander"], ".")
+        tf_processor_node = combine_names([namespace, "tf_processor"], ".")
+        ld += [
+            Node(package="robot_command", executable="commander",
+                namespace=namespace, output="screen",
+                arguments=[
+                    "--ros-args", "--log-level", f"{commander_node}:={log_level}"
+                ]),
+            Node(package="robot_command", executable="tf_processor",
+                namespace=namespace, output="screen",
+                arguments=[
+                    "--ros-args", "--log-level", f"{tf_processor_node}:={log_level}"
+                ])
+        ]
     return ld
