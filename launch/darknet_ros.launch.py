0785eb734ca6e9601fe4a3245460918b063bdcb8
Blake McHale <mchale.blake@gmail.com>
2024-06-13 08:47:34 -0700

    Use LaunchManager

--- launch/darknet_ros.launch.py
+++ launch/darknet_ros.launch.py
@@ -4,16 +4,71 @@ darknet_ros.launch.py
 
 Runs specified number of vehicles in simulation with controller.
 '''
-from launch import LaunchDescription
-from launch.actions import OpaqueFunction
-from robot_bringup.launch.darknet_ros import launch_setup, LAUNCH_ARGS
-from ros2_utils.launch import get_launch_arguments
+from ros2_utils.launch_manager import LaunchManager
+from launch_ros.actions.node import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 
-# https://github.com/colcon/colcon-core/issues/169
+# darknet_ros_share = get_package_share_directory("darknet_ros")
+# robot_bringup_share = get_package_share_directory("robot_bringup")
+darknet_ros_share = ""
+robot_bringup_share = ""
+
+YOLO_MAIN_PATH = os.path.join(darknet_ros_share,"yolo_network_config")
+YOLO_WEIGHTS_DEFAULT = os.path.join(YOLO_MAIN_PATH,"weights")
+YOLO_CONFIG_DEFAULT = os.path.join(YOLO_MAIN_PATH,"cfg")
+ROS_PARAM_DEFAULT = os.path.join(robot_bringup_share,"config","darknet_ros.yaml")
+NETWORK_PARAM_DEFAULT = os.path.join(darknet_ros_share,"config","yolov4-tiny.yaml")
+
+
+def launch_setup(args):
+    lm = LaunchManager()
+    ns = ""
+    if args.namespace:
+        ns = f"/{args.namespace}"
+    # lm.add_action(
+    #     Node(
+    #         namespace=f'{args.namespace}/darknet_ros',
+    #         package='darknet_ros',
+    #         executable='darknet_ros',
+    #         name='darknet_ros',
+    #         output=args.output,
+    #         parameters=[args.ros_param_file, args.network_param_file,
+    #         {
+    #             "config_path": args.yolo_config_path, 
+    #             "weights_path": args.yolo_weights_path,
+    #         },
+    #         ],
+    #         # arguments=[
+    #         #             "--ros-args", "--log-level", f"darknet_ros:=DEBUG"
+    #         # ],
+    #         remappings=[
+    #             ("/camera/image_raw", f"{ns}/realsense/color/image_raw"),
+    #             ("/darknet_ros/bounding_boxes", f"{ns}/darknet_ros/bounding_boxes"),
+    #             ("/darknet_ros/detection_image", f"{ns}/darknet_ros/detection_image"),
+    #             ("/darknet_ros/found_object", f"{ns}/darknet_ros/found_object")
+    #         ]
+    #     )
+    # )
+    lm.add_action(
+        Node(
+            namespace=args.namespace,
+            package="cv_ros",
+            executable="create_bb_tf",
+            output=args.output,
+        )
+    )
+    return lm.describe_sub_entities()
+
+
 def generate_launch_description():   
-    launch_description = []
-    launch_description += get_launch_arguments(LAUNCH_ARGS)
-    launch_description += [OpaqueFunction(function = launch_setup)] 
-    return LaunchDescription(launch_description)
-
+    lm = LaunchManager()
+    lm.add_arg("yolo_weights_path", YOLO_WEIGHTS_DEFAULT, "Path to YOLO weights.")
+    lm.add_arg("yolo_config_path", YOLO_CONFIG_DEFAULT, "Path to YOLO config.")
+    lm.add_arg("ros_param_file", ROS_PARAM_DEFAULT, "Path to ROS params.")
+    lm.add_arg("network_param_file", NETWORK_PARAM_DEFAULT, "Path to network params.")
+    lm.add_arg("namespace", "drone_0", "Namespace of node.")
+    lm.add_arg("output", "screen", "Where to output darknet and bb tf creation.")
+    lm.add_opaque_function(launch_setup)
+    return lm
--- launch/run_vehicles.launch.py
+++ launch/run_vehicles.launch.py
@@ -4,15 +4,66 @@ run_vehicles.launch.py
 
 Runs specified number of vehicles in simulation with controller.
 '''
-from launch import LaunchDescription
-from launch.actions import OpaqueFunction
-from robot_bringup.launch.run_vehicles import launch_setup, LAUNCH_ARGS
-from ros2_utils.launch import get_launch_arguments
+from ros2_utils.launch_manager import LaunchManager
+from launch_ros.actions.node import Node
+from ament_index_python.packages import get_package_share_directory
+from ros2_utils.launch import combine_names
+from robot_control.launch.run_vehicles import get_team
+import os
+
+
+ROBOT_COMMAND_PKG = get_package_share_directory("robot_command")
+ROBOT_BRINGUP_PKG = get_package_share_directory("robot_bringup")
+
+
+def launch_setup(args):
+    lm = LaunchManager()
+    lm.add_include_launch_description("robot_control", "run_vehicles.launch.py")
+
+    team = get_team(args, args.context)
+    log_level = args.log_level.upper()
+    tf_processor_args = []
+    for namespace, vehicle_args in team.items():
+        commander_node = combine_names([namespace, "commander"], ".")
+        lm.add_action(
+            Node(package="robot_command", executable="commander",
+                namespace=namespace, output="screen",
+                arguments=[
+                    "--ros-args", "--log-level", f"{commander_node}:={log_level}"
+                ])
+        )
+        if "tf_processor" in vehicle_args and isinstance(vehicle_args["tf_processor"], dict) and vehicle_args["tf_processor"]:
+            for k, v in vehicle_args["tf_processor"].items():
+                if not k.startswith("/"): tf_processor_args += ["--tf-pair", f"{namespace}/{k},{v}"]
+        # if vehicle_args["use_darknet"]:
+        #     launch_arguments=[
+        #         ('namespace', namespace),
+        #         ('output', 'log'),
+        #     ]
+        #     lm.add_include_launch_description("robot_bringup", "darknet_ros.launch.py", launch_arguments)
+    if tf_processor_args:
+        tf_processor_node = combine_names(["tf_processor"], ".")
+        tf_processor_args += ["--ros-args", "--log-level", f"{tf_processor_node}:={log_level}",]
+        lm.add_action(
+            Node(package="robot_command", executable="tf_processor",
+            output="screen",
+            arguments=tf_processor_args),
+        )
+    if args.use_control_center:
+        lm.add_action(
+            Node(package="robot_command", executable="control_center",
+            namespace="hq",
+            output="screen"),
+        )
+    return lm.describe_sub_entities()
 
 
 # https://github.com/colcon/colcon-core/issues/169
-def generate_launch_description():   
-    launch_description = []
-    launch_description += get_launch_arguments(LAUNCH_ARGS)
-    launch_description += [OpaqueFunction(function = launch_setup)] 
-    return LaunchDescription(launch_description)
+def generate_launch_description():
+    lm = LaunchManager()
+    lm.add_arg("use_darknet", "False", "Flag for turning on darknet nodes and capabilities on single drone.")
+    lm.add_arg("use_control_center", "False", "Flag for turning on control center capabilities.")
+    lm.add_include_launch_args("robot_control", "run_vehicles.launch.py")
+    # lm.add_include_launch_args("robot_bringup", "darknet_ros.launch.py")
+    lm.add_opaque_function(launch_setup, yaml_file=os.environ.get("ROBOT_CONTROL_CONFIG", ""))
+    return lm
